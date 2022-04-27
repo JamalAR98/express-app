73518d8f118c6170ae2bf90518b11c090a633f72
Jamal AMRINS <62074649+JamalAR98@users.noreply.github.com>
2022-04-27 07:10:47 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = module.require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/add', (req, res) => {
+    res.json({msg : "Test"})
+})
+
+app.listen(PORT, () => {
+    console.log('server is ON');
+})
+
+
